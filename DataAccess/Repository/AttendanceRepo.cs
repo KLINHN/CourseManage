23a9e0fefa127144f66c41a48b309979bc5ff662
khanhlinh00 <92656472+KLINHN@users.noreply.github.com>
2023-08-22 11:37:25 +0700

    add repo

---
+++ DataAccess/Repository/AttendanceRepo.cs
@@ -0,0 +1,14 @@
+﻿using BusinessData.Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class AttendanceRepo : RepositoryBase<Attendance>
+    {
+    }
+}
